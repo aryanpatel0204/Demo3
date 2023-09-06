b73bef21e349b5abe574970d242af51322af5a67
Aryan Patel <144089245+aryanpatel0204@users.noreply.github.com>
2023-09-06 17:48:48 +0530

    Delete News directory

--- News/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from News import views
-
-urlpatterns = [
-    path('',views.news,name='news'),
-]
--- News/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from .models import *
-
-# Create your views here.
-def news(request):
-    new=newss.objects.all().order_by('-date')
-    return render(request,'news.html',{'new':new})
