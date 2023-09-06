41776013abe18ab58a4b191bcd03f4b354a598e0
Aryan Patel <144089245+aryanpatel0204@users.noreply.github.com>
2023-09-06 17:49:02 +0530

    Delete accounts directory

--- accounts/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from .models import *
-# Create your views here.
-def signup(request):
-    sign=Signup.objects.all()
-    return render(request,'SignUp.html',{'sign':sign})
-def login(request):
-    user=Signup.objects.all()
-    return render(request,'login.html',{'user':user})
