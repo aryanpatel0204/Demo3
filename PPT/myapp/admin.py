5927e563af0b1056c44d54e4a9c0a396cd501a5f
Aryan Patel <144089245+aryanpatel0204@users.noreply.github.com>
2023-09-16 10:48:53 +0530

    Delete PPT directory

--- PPT/myapp/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import *
-#here . means u are in same folder.
-# Register your models here.
-admin.site.register(Movie)
--- PPT/myapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from myapp import views
-
-
-urlpatterns = [
-    path('',views.home),
-    path('room/',views.room),
-]
